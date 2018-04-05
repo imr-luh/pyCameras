9ef216f31457c0f83f92895f5baea867098b1de5
Niklas Kröger <niklas.kroeger@imr.uni-hannover.de>
2018-04-05 14:34:03 +0200

    Add docstrings to util functions
    and rename getAllAvailableDevices to getAllDevices
    
    Related to #7

--- pyCameras/utils.py
+++ pyCameras/utils.py
@@ -41,6 +41,14 @@ def dynamic_import_class(name):
 
 
 def listCameraImplementations():
+    """
+    Find all classes with the name `Camera` in the pyCameras project
+
+    Returns
+    -------
+    classes : list
+        List of all class implementations with the name `Camera`
+    """
     package = pyCameras
     classes = []
     for importer, modname, ispkg in pkgutil.walk_packages(
@@ -59,6 +67,17 @@ def listCameraImplementations():
     return classes
 
 
-def getAllAvailableDevices():
+def getAllDevices():
+    """
+    Find all available camera devices that are reachable right now
+
+    Returns
+    -------
+    devices : dict
+        Dict of all available camera devices that are reachable at the moment.
+        The devices are separated by their respective class implementation.
+        The implementation.__module__ string is the dict key under which a list
+        of the respective devices is returned.
+    """
     return {implementation.__module__: implementation.listDevices() for
             implementation in listCameraImplementations()}
